1a11cd5587077120c236d93738ec3c49f35dbf80
Young <young@perkd.me>
2025-02-20 13:55:18 +0800

    rules

--- test/quick-verification.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-const utils = require('../lib/utils');
-
-// Verify native promise usage only
-const cb = utils.createPromiseCallback();
-assert(cb.promise instanceof Promise, 'Should create native promise'); 
